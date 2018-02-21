fe5794eb004f62e19de3cf43ac2e7ef2f10bc623
Deby Lepage <db0company@gmail.com>
2018-02-21 10:49:54 +0800

    Fix javascript to modify form to make sure it works when the filter side bar exists on the page

--- js/main.js
+++ js/main.js
@@ -37,70 +37,71 @@ function loadSongItem() {
 }
 
 function cuteformclearOne(cuteform) {
-    $('#' + cuteform).show();
-    $('#' + cuteform + ' + .cuteform').remove();
+    cuteform.show();
+    cuteform.next('.cuteform').remove();
 }
 
 function loadCardForm() {
+    var form = $('[data-form-name="edit_card"], [data-form-name="add_card"]');
     function showVariable(k, v) {
-        $('#id_skill_' + v).closest('.form-group').show();
-        $('#id_i_skill_' + v).closest('.form-group').show();
+        form.find('#id_skill_' + v).closest('.form-group').show();
+        form.find('#id_i_skill_' + v).closest('.form-group').show();
     }
     function onSkillChange() {
-        let selectedSkill = $('#id_i_skill_type').val();
-        let selectedSideSkill = $('#id_i_side_skill_type').val();
-        let selectedSpecial = $('#id_i_skill_special').val();
+        let selectedSkill = form.find('#id_i_skill_type').val();
+        let selectedSideSkill = form.find('#id_i_side_skill_type').val();
+        let selectedSpecial = form.find('#id_i_skill_special').val();
         // Hide all
-        $('#id_i_side_skill_type').closest('.form-group').hide();
-        $('#id_i_skill_type').closest('.form-group').find('.alert').hide();
-        $('#id_i_side_skill_type').closest('.form-group').find('.alert').hide();
-        $('#id_i_skill_special').closest('.form-group').hide();
+        form.find('#id_i_side_skill_type').closest('.form-group').hide();
+        form.find('#id_i_skill_type').closest('.form-group').find('.alert').hide();
+        form.find('#id_i_side_skill_type').closest('.form-group').find('.alert').hide();
+        form.find('#id_i_skill_special').closest('.form-group').hide();
         $.each(all_variables, function(k, v) {
-            $('#id_skill_' + v).closest('.form-group').hide();
-            $('#id_i_skill_' + v).closest('.form-group').hide();
+            form.find('#id_skill_' + v).closest('.form-group').hide();
+            form.find('#id_i_skill_' + v).closest('.form-group').hide();
         });
         // Show special case if exists
         if (special_cases_variables[selectedSkill]) {
-            $('#id_i_skill_special').closest('.form-group').show();
+            form.find('#id_i_skill_special').closest('.form-group').show();
         }
         // Main skill
         if (selectedSkill != '') {
             // Show side if main is selected
-            $('#id_i_side_skill_type').closest('.form-group').show();
+            form.find('#id_i_side_skill_type').closest('.form-group').show();
             // Show template + variables
             if (selectedSpecial != '' && special_cases_variables[selectedSkill]) {
                 // Special case
                 $.each(special_cases_variables[selectedSkill][selectedSpecial], showVariable);
-                $('#id_i_skill_type').closest('.form-group').find('.alert').html(
+                form.find('#id_i_skill_type').closest('.form-group').find('.alert').html(
                     special_cases_template[selectedSkill][selectedSpecial],
                 );
             } else {
                 // Normal case
                 $.each(variables_per_skill_type['skill'][selectedSkill], showVariable);
-                $('#id_i_skill_type').closest('.form-group').find('.alert').html(
+                form.find('#id_i_skill_type').closest('.form-group').find('.alert').html(
                     template_per_skill_type['skill'][selectedSkill],
                 );
             }
-            $('#id_i_skill_type').closest('.form-group').find('.alert').show();
+            form.find('#id_i_skill_type').closest('.form-group').find('.alert').show();
             // Side skill template + variable
             if (selectedSideSkill != '') {
                 $.each(variables_per_skill_type['side_skill'][selectedSideSkill], showVariable);
-                $('#id_i_side_skill_type').closest('.form-group').find('.alert').html(
+                form.find('#id_i_side_skill_type').closest('.form-group').find('.alert').html(
                     template_per_skill_type['side_skill'][selectedSideSkill],
                 );
-                $('#id_i_side_skill_type').closest('.form-group').find('.alert').show();
+                form.find('#id_i_side_skill_type').closest('.form-group').find('.alert').show();
             }
         }
     }
-    cuteformclearOne('id_i_skill_type');
+    cuteformclearOne(form.find('#id_i_skill_type'));
     // Add alert
-    $('#id_i_skill_type').closest('.form-group').find('.alert').remove;
-    $('#id_i_skill_type').after('<div class="alert alert-info" style="margin-top: 10px;"></div>');
-    $('#id_i_side_skill_type').closest('.form-group').find('.alert').remove;
-    $('#id_i_side_skill_type').after('<div class="alert alert-info" style="margin-top: 10px;"></div>');
+    form.find('#id_i_skill_type').closest('.form-group').find('.alert').remove;
+    form.find('#id_i_skill_type').after('<div class="alert alert-info" style="margin-top: 10px;"></div>');
+    form.find('#id_i_side_skill_type').closest('.form-group').find('.alert').remove;
+    form.find('#id_i_side_skill_type').after('<div class="alert alert-info" style="margin-top: 10px;"></div>');
     // Trigger on skill change
     onSkillChange();
-    $('#id_i_skill_type').change(onSkillChange);
-    $('#id_i_side_skill_type').change(onSkillChange);
-    $('#id_i_skill_special').change(onSkillChange);
+    form.find('#id_i_skill_type').change(onSkillChange);
+    form.find('#id_i_side_skill_type').change(onSkillChange);
+    form.find('#id_i_skill_special').change(onSkillChange);
 }

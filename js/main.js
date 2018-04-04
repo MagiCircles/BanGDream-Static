5e7d498d8abd34d9ba7fe4e348b2d9c1a9b90e51
db0company <db0company@gmail.com>
2018-04-04 13:25:09 +0800

    Condensed version of gacha/event with toggler to display details. Current/future events/gacha will be opened by default. Some fields changed their position in the list. Events title fields ot merged into one (Translated + Japanese together).

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
+// *****************************************
+// Utils
 
-function loadProfileComingSoon(tab_name, user_id, onDone) {
-    onDone('<div class="padding20"><div class="alert alert-info text-center"><i class="flaticon-idolized"></i> ' + gettext('Coming soon') + ' <i class="flaticon-idolized"></i></div></div>');
+function cuteformclearOne(cuteform) {
+    cuteform.show();
+    cuteform.next('.cuteform').remove();
 }
 
-function loadAccountComingSoon(tab_name, user_id, account_id, onDone) {
-    onDone('<div class="alert alert-info text-center"><i class="flaticon-idolized"></i> ' + gettext('Coming soon') + ' <i class="flaticon-idolized"></i></div>');
-}
+// *****************************************
+// Cards
 
-
+// Card item
 function loadCard() {
     $('[data-open-tab]').each(function() {
 	$(this).unbind('click');
@@ -19,6 +21,7 @@ function loadCard() {
     });
 }
 
+// Card list
 function loadCardInList() {
     var swap = function() {
         var newSource = $(this).data('trained');
@@ -28,35 +31,7 @@ function loadCardInList() {
     $('.card-solo').hover(swap, swap);
 }
 
-function loadSongItem() {
-    let height = $('.song-info .song-title-section').height();
-    if (height > 0) {
-        $('.song-info .top-item .song-image').css('max-height', height);
-    }
-    loadAlliTunesData();
-}
-
-function cuteformclearOne(cuteform) {
-    cuteform.show();
-    cuteform.next('.cuteform').remove();
-}
-
-function loadCollecticleCardForm() {
-    var trainedCheckbox = $('[data-form-name$="_collectiblecard"] #id_trained');
-    var preferUntrainedCheckbox = $('[data-form-name$="_collectiblecard"] #id_prefer_untrained');
-    var preferUntrainedField = preferUntrainedCheckbox.closest('.form-group');
-    function showHidePreferUntrained() {
-        if (trainedCheckbox.prop('checked') === true) {
-            preferUntrainedField.show();
-        } else {
-            preferUntrainedCheckbox.prop('checked', false);
-            preferUntrainedField.hide();
-        }
-    }
-    showHidePreferUntrained();
-    trainedCheckbox.change(showHidePreferUntrained);
-}
-
+// Card form
 function loadCardForm() {
     var form = $('[data-form-name="edit_card"], [data-form-name="add_card"]');
     function showVariable(k, v) {
@@ -123,4 +98,87 @@ function loadCardForm() {
     form.find('#id_i_skill_type').change(onSkillChange);
     form.find('#id_i_side_skill_type').change(onSkillChange);
     form.find('#id_i_skill_special').change(onSkillChange);
+}
+
+// Collectible card form
+function loadCollecticleCardForm() {
+    var trainedCheckbox = $('[data-form-name$="_collectiblecard"] #id_trained');
+    var preferUntrainedCheckbox = $('[data-form-name$="_collectiblecard"] #id_prefer_untrained');
+    var preferUntrainedField = preferUntrainedCheckbox.closest('.form-group');
+    function showHidePreferUntrained() {
+        if (trainedCheckbox.prop('checked') === true) {
+            preferUntrainedField.show();
+        } else {
+            preferUntrainedCheckbox.prop('checked', false);
+            preferUntrainedField.hide();
+        }
+    }
+    showHidePreferUntrained();
+    trainedCheckbox.change(showHidePreferUntrained);
+}
+
+// *****************************************
+// Song
+
+function loadSongItem() {
+    let height = $('.song-info .song-title-section').height();
+    if (height > 0) {
+        $('.song-info .top-item .song-image').css('max-height', height);
+    }
+    loadAlliTunesData();
+}
+
+// *****************************************
+// Events / Gachas
+
+function loadEventGacha() {
+    function toggleVersion(version, prefix, toggle, animation) {
+        let caret = $('[data-field="' + prefix + 'image"] .glyphicon');
+        let isOpen = $('[data-field="' + prefix + 'countdown"]').length > 0;
+        if (toggle) {
+            if (caret.hasClass('glyphicon-triangle-bottom')) {
+                caret.removeClass('glyphicon-triangle-bottom');
+                caret.addClass('glyphicon-triangle-top');
+            } else {
+                caret.removeClass('glyphicon-triangle-top');
+                caret.addClass('glyphicon-triangle-bottom');
+            }
+        } else {
+            if (isOpen) {
+                caret.removeClass('glyphicon-triangle-bottom');
+                caret.addClass('glyphicon-triangle-top');
+            } else {
+                caret.removeClass('glyphicon-triangle-top');
+                caret.addClass('glyphicon-triangle-bottom');
+            }
+        }
+        $.each(fields_per_version, function(_, field_name) {
+            let field = $('[data-field="' + prefix + field_name + '"]');
+            if (field_name != 'image') {
+                if (toggle) {
+                    field.toggle(animation);
+                } else {
+                    if (isOpen) {
+                        field.show(animation);
+                    } else {
+                        field.hide(animation);
+                    }
+                }
+            }
+        });
+    }
+    if (typeof versions_prefixes != 'undefined' && typeof fields_per_version != 'undefined') {
+        $.each(versions_prefixes, function(version, prefix) {
+            let field = $('[data-field="' + prefix + 'image"]');
+            field.find('th').first().append('&nbsp;&nbsp;<span class="glyphicon glyphicon-triangle-bottom"></span>');
+            field.css('cursor', 'pointer');
+            field.unbind('click');
+            field.click(function(e) {
+                e.preventDefault();
+                toggleVersion(version, prefix, true, 'fast');
+                return false;
+            });
+            toggleVersion(version, prefix, false);
+        });
+    }
 }
